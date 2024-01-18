a32d7a78c245acc02dc8864077703723a7496027
Baxtiyor1990 <156651092+Baxtiyor1990@users.noreply.github.com>
2024-01-18 16:22:45 +0500

    Add docstring to convert_to_uppercase function

--- str_func.py
+++ str_func.py
@@ -1,1 +1,13 @@
-def convert_to_uppercase(input_str):\n    return input_str.upper()
+
+def convert_to_uppercase(input_str):
+    """
+    Converts the input string to uppercase.
+
+    Args:
+        input_str (str): The input string to be converted.
+
+    Returns:
+        str: The input string converted to uppercase.
+    """
+    return input_str.upper()
+
